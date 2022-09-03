ff07baedb01ce35ffa1d94d3d6c3c8d6ab3d51eb
Sophie <clark.sophie2000@gmail.com>
2022-09-03 18:29:35 +0100

    Update bubble sort with shine on two items getting compared
    some validation for input and buttons also

--- src/App.js
+++ src/App.js
@@ -13,8 +13,10 @@ class App extends Component {
           passes : 0,
           sorting: false,
           currentIndex: 0,
-          isVisible : true,
-          disabled: false
+          isAddButtonVisible : true,
+          isStartButtonDisabled : true,
+          disabled: false,
+          swapped: false
       };
   }
 
@@ -30,57 +32,80 @@ class App extends Component {
 
   }
 
-  handleClick = () => {
-      this.setState({
-        isVisible: false
-      })
-      this.setState({
-        disabled: true
-      })
-      if (this.state.sorting) {
-
-          return false
-      } else {
-          this.sort()
+  inputValidation() {
+    let validInput = true;
+    var re = new RegExp("^([a-zA-Z0-9]){1,9}$");
+    this.state.list.some(function (item) {
+      if (!re.test(item)) {
+        return validInput =  false;
       }
+    });
+    return validInput;
   }
 
-  sort = () => {
-      if (this.state.currentIndex === this.state.len - 1) {
+  handleStartClick = () => {
+    if (!this.inputValidation())
+      return alert("Please make sure inputs is filled out correctly.");
+    if (this.state.sorting) {
+        return false;
+    } else {
         this.setState({
+          isAddButtonVisible: false,
+          disabled: true,
+          isStartButtonDisabled: true
+        })
+        this.sort();
+    }
+  }
+
+  finished = () => {
+    this.setState({ 
+      sorting: false,
+      passes: this.state.passes + 1,
+      isStartButtonDisabled: true
+     });
+     alert('Finished Sorting');
+  }
+  sort = () => {
+      if (this.state.currentIndex === this.state.len - 1) {  
+          if (this.state.swapped === false) {
+            return this.finished();
+          }
+          this.setState({
             currentIndex: 0,
             passes: this.state.passes + 1,
-            len: this.state.len - 1
-      })
+            len: this.state.len - 1,
+            swapped: false
+          })
       }
-      if (this.state.len === 1) {
-          alert('Finished Sorting');
-          this.setState({ sorting: false });
-          return;
-      }
+      if (this.state.len === 1)
+        return this.finished();
       this.setState({ sorting: true })
       let swapped = false;
-      let currentIndex = this.state.currentIndex
+      let currentIndex = this.state.currentIndex;
       let list = this.state.list;
+      this.applyComparisonIndicator();
       if (list[currentIndex] > list[currentIndex + 1]) {
           this.applyHighlightToSwappingItems();
           let temp = list[currentIndex];
           list[currentIndex] = list[currentIndex + 1];
           list[currentIndex + 1] = temp;
-          this.setState({ swaps: this.state.swaps + 1 });
-          this.setState({ list: list })
           swapped = true;
+          this.setState({ 
+            swaps: this.state.swaps + 1,
+            list: list,
+            swapped: swapped
+          });
       }
-      currentIndex ++;
       this.setState({ currentIndex: this.state.currentIndex + 1 });
       if (swapped)
         setTimeout(this.removeHighlightFromSwappedItems, 1000);
+      setTimeout(this.removeComparisonIndicator, 1000);
       setTimeout(this.sort, 2000);
   }
 
   removeHighlightFromSwappedItems = () => {
-    document.querySelector("#text-" + this.state.currentIndex).classList.remove('highlight');
-    document.querySelector("#text-" + (this.state.currentIndex - 1)).classList.remove('highlight');
+    document.querySelectorAll('input').forEach((el) => el.classList.remove('highlight'));
   }
 
   applyHighlightToSwappingItems() {
@@ -88,11 +113,24 @@ class App extends Component {
     document.querySelector("#text-" + (this.state.currentIndex + 1)).classList.add('highlight');
   }
 
+  applyComparisonIndicator() {
+    document.querySelector("#bubble-" + this.state.currentIndex).classList.add('comparison');
+    document.querySelector("#bubble-" + (this.state.currentIndex + 1)).classList.add('comparison');
+  }
+
+  removeComparisonIndicator = () => {
+    document.querySelectorAll('div').forEach((el) => el.classList.remove('comparison'));
+  }
+
+  refreshClick() {
+    window.location.reload();
+  }
+
   render() {
       return (
           <div className='container'>
              <h1>Bubble Sort</h1>
-             <h3>Add numbers or characters A-Z, then press begin to bubble sort them.</h3>
+             <h3>Add numbers or characters A-z (limit 8), then press Begin to bubble sort them.</h3>
              <div className='list-container'>
               {
                 this.state.list.map((item, index) => {
@@ -105,12 +143,13 @@ class App extends Component {
               }
              </div>
              {
-               this.state.isVisible?
-               <button onClick={(e)=>this.addItem(e)}>Add Item</button>
-               :null
+               <button className="add" onClick={(e) => this.addItem(e)} disabled = {(this.state.isAddButtonVisible && this.state.len < 8 ? false : true )}>Add Item</button>
              }
-             
-              <button onClick={this.handleClick}>Begin</button>
+             {
+              <button className="start" onClick={this.handleStartClick} disabled = {(this.state.isStartButtonDisabled && this.state.len < 3) ? true : false } >Begin</button>
+             }
+
+              <button className="reset" onClick={this.refreshClick} >Reset</button>
               <p>swaps:  {this.state.swaps}</p>
               <p>passes: {this.state.passes}</p>
           </div>
